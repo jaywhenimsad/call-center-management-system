7cf4796c7fefaee53fbe226b8a74f2d390787689
jaywhenimsad <jaykumar.rprhd22@sinhgad.edu>
2024-07-27 12:20:01 +0530

    Initial commit

---
+++ src/main/java/com/example/call_center_management_system/CallCenterManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.call_center_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCenterManagementSystemApplication.class, args);
+
+	}
+
+
+}
---
+++ src/test/java/com/example/call_center_management_system/CallCenterManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.call_center_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallCenterManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
